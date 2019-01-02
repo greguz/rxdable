6420a8f325845e1e54e1262becc1bf90b130c1e9
greguz <gregolettogiacomo@gmail.com>
2019-01-02 17:53:27 +0100

    Add writable subscription test

--- src/subscribeToStream.spec.ts
+++ src/subscribeToStream.spec.ts
@@ -1,17 +1,19 @@
 import "mocha";
 import { expect } from "chai";
 
-import { createReadStream } from "fs";
+import * as os from "os";
+import * as path from "path";
+import * as fs from "fs";
 import { Observable } from "rxjs";
 import { toArray } from "rxjs/operators";
 
 import { subscribeToStream } from "./subscribeToStream";
 
 describe("subscribeToStream", () => {
-  it("should work", async () => {
+  it("should work with readables", async () => {
     const observable = new Observable<string>(subscriber =>
       subscribeToStream(
-        createReadStream(__filename, "utf8"),
+        fs.createReadStream(__filename, "utf8"),
         chunk => subscriber.next(chunk),
         error => subscriber.error(error),
         () => subscriber.complete()
@@ -21,6 +23,28 @@ describe("subscribeToStream", () => {
     const chunks = await observable.pipe(toArray()).toPromise();
 
     const text = chunks.join("");
+
+    expect(text).to.match(/But in the end, it doesn't even matter/);
+  });
+
+  it("should work with writables", async () => {
+    const file = path.join(os.tmpdir(), "subscribeToStream.test");
+    const encoding = "utf8";
+
+    await new Promise((resolve, reject) =>
+      subscribeToStream(
+        fs
+          .createReadStream(__filename, encoding)
+          .pipe(fs.createWriteStream(file, encoding)),
+        null,
+        reject,
+        resolve
+      )
+    );
+
+    const text = fs.readFileSync(file, encoding);
+
+    fs.unlinkSync(file);
 
     expect(text).to.match(/But in the end, it doesn't even matter/);
   });
