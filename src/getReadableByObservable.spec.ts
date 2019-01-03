86f6e5e1e79dfe0a50163e369231d137da37908f
greguz <gregolettogiacomo@gmail.com>
2019-01-03 16:12:36 +0100

    Add more Rxdable tests

--- src/getReadableByObservable.spec.ts
+++ src/getReadableByObservable.spec.ts
@@ -1,35 +1,123 @@
 import "mocha";
 import { expect } from "chai";
 
-import * as path from "path";
-import * as os from "os";
-import { createWriteStream, readFileSync, unlinkSync } from "fs";
-import { from } from "rxjs";
+import { fill } from "lodash";
+import { Writable } from "stream";
+import { from, Observable } from "rxjs";
 
 import { getReadableByObservable } from "./getReadableByObservable";
 
 describe("getReadableByObservable", () => {
-  it("should work", async () => {
-    const file = path.join(os.tmpdir(), "getReadableByObservable.test");
-    const encoding = "utf8";
-    const observable = from([
-      "It's fun to stay at the ",
-      "Y.",
-      "M.",
-      "C.",
-      "A."
-    ]);
+  it("should work", done => {
+    // Fast-firing observable
+    const observable = from(fill(new Array(50), "x"));
 
-    await new Promise((resolve, reject) => {
-      getReadableByObservable(observable)
-        .pipe(createWriteStream(file, { encoding }))
-        .once("error", reject)
-        .once("finish", resolve);
+    // Cast to readable
+    const readable = getReadableByObservable(observable);
+    expect(readable.subscription).to.equal(null);
+
+    // Slow-ass writable
+    const writable = new Writable({
+      objectMode: true,
+      write(chunk, encoding, callback) {
+        setTimeout(() => callback(), 10);
+      }
     });
 
-    const content = readFileSync(file, "utf8");
-    unlinkSync(file);
+    // Clean-up utility
+    function complete(error?: any) {
+      readable.destroy();
+      writable.destroy();
+      done(error);
+    }
 
-    expect(content).to.be.equal("It's fun to stay at the Y.M.C.A.");
+    // Watch streams
+    readable.once("error", complete);
+    writable.once("error", complete);
+    writable.once("finish", complete);
+
+    // Start data flow
+    readable.pipe(writable);
+  });
+
+  it("should handle observable errors", done => {
+    // Observable that will explode
+    const observable = new Observable<number>(subscriber => {
+      setTimeout(() => subscriber.error(new Error("Kill 'Em All")), 10);
+    });
+
+    // Convert to readable
+    const readable = getReadableByObservable(observable);
+
+    // Empty writable
+    const writable = new Writable({
+      objectMode: true,
+      write(chunk, encoding, callback) {
+        callback();
+      }
+    });
+
+    // Clean-up utility
+    function complete(error?: any) {
+      readable.destroy();
+      writable.destroy();
+      if (error instanceof Error && error.message === "Kill 'Em All") {
+        done();
+      } else {
+        done(error);
+      }
+    }
+
+    // Watch streams
+    readable.once("error", complete);
+    writable.once("error", complete);
+    writable.once("finish", complete);
+
+    // Start data flow
+    readable.pipe(writable);
+  });
+
+  it("should unsubscribe from the observable", function(done) {
+    // Subscription status
+    let unsubscribed = false;
+
+    // Infinite observable
+    const observable = new Observable<number>(subscriber => {
+      let i = 0;
+      const timer = setInterval(() => {
+        subscriber.next(i++);
+      }, 100);
+      return function unsubscribe() {
+        unsubscribed = true;
+        clearInterval(timer);
+      };
+    });
+
+    // Convert to readable
+    const readable = getReadableByObservable(observable);
+
+    // Will-explode writable
+    const writable = new Writable({
+      objectMode: true,
+      write(chunk, encoding, callback) {
+        callback(chunk >= 10 ? new Error("Kill 'Em All") : null);
+      }
+    });
+
+    // Clean-up utility
+    function complete() {
+      readable.destroy();
+      writable.destroy();
+      expect(unsubscribed).to.equal(true);
+      done();
+    }
+
+    // Watch streams
+    readable.once("error", complete);
+    writable.once("error", complete);
+    writable.once("finish", complete);
+
+    // Start data flow
+    readable.pipe(writable);
   });
 });
